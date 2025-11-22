53e1b0780e2825ffdef8e2124e09f324101f4a1e
Aaron0626 <dora9402@gmail.com>
2025-11-23 01:50:17 +0800

    Fix nested repository

---
+++ 01257129-HW4/01257129-HW4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01257129-HW4
+//
+//  Created by user10 on 2025/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01257129-HW4/01257129-HW4/_1257129_HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  _1257129_HW4App.swift
+//  01257129-HW4
+//
+//  Created by user10 on 2025/11/21.
+//
+
+import SwiftUI
+
+@main
+struct _1257129_HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
